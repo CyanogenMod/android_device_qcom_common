e8876e3d937824ecf336a79ecc373e25ce5f21ef
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2011-06-29 20:13:00 +0530

    Build: call vendor specific script
    Change-Id: I1f41d9ca6145eb13b35a19428c13b9a678a3153f

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,8 @@
-# vendor/qcom/proprietary/prebuilt contains prebuilt binaries for
-# a few modules in vendor/qcom/prioprietary. This script is invokde
-# during source/envsetup.sh. It copies binaries from vendor/qcom/
-# proprietary/prebuilt to croot/out.
-
-if [ ! -d out ]; then
-  mkdir out
-fi
+# This script is invoked during source/envsetup.sh.
+# It sets vendor specific environment for build.
 
 if [ -d "vendor/qcom/proprietary/prebuilt_HY11" ]; then
-  cp -rf vendor/qcom/proprietary/prebuilt_HY11/* out
-fi
-
-if [ -d "vendor/qcom/proprietary/prebuilt_HY22" ]; then
-  cp -rf vendor/qcom/proprietary/prebuilt_HY22/* out
+  . vendor/qcom/proprietary/common/build/vendorsetup.sh
+elif [ -d "vendor/qcom/proprietary/prebuilt_HY22" ]; then
+  . vendor/qcom/proprietary/common/build/vendorsetup.sh
 fi
