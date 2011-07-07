90665c2625fdc82050a804c24376b648035ca00d
Nagender Telkar <ntelkar@codeaurora.org>
2011-07-07 15:16:45 -0600

    Build : Delete vendorsetup.sh
    This file become obsolete - as the invocation of vendorsetup.sh
    is moved to device-vendor.mk. Hence, deleting the file.
    
    Change-Id: I5e4d15b1d39b9f1e2e0239043bd1c8fb6a18597c

--- vendorsetup.sh
+++
@@ -1,8 +0,0 @@
-# This script is invoked during source/envsetup.sh.
-# It sets vendor specific environment for build.
-
-if [ -d "vendor/qcom/proprietary/prebuilt_HY11" ]; then
-  . vendor/qcom/proprietary/common/build/vendorsetup.sh
-elif [ -d "vendor/qcom/proprietary/prebuilt_HY22" ]; then
-  . vendor/qcom/proprietary/common/build/vendorsetup.sh
-fi
