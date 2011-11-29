28a955ca45f0b3cfe36b7e072890aaf944879262
Shashank Mittal <mittals@codeaurora.org>
2011-11-28 18:53:10 -0800

    qcom/common: Add key layout file for cyttsp-i2c device on msm8960
    Change-Id: Ia3f94647b09b87f7c1b749d63989f21020356221

--- common.mk
+++ common.mk
@@ -141,6 +141,7 @@ KEYPAD += 8660_handset.kl
 KEYPAD += ffa-keypad.kl
 KEYPAD += fluid-keypad.kl
 KEYPAD += keypad_8960.kl
+KEYPAD += cyttsp-i2c.kl
 KEYPAD += msm8960-snd-card_Button_Jack.kl
 KEYPAD += msm_tma300_ts.kl
 KEYPAD += surf_keypad.kl
