e2c1872061128d1eb19e8be72c876b337c5d50a1
Ajay Dudani <adudani@codeaurora.org>
2012-01-26 20:51:17 -0800

    common: Drop advertising NFC support
    Reference devices do not have NFC support, so dropping it from common
    list of advertised capabilities.
    
    Change-Id: I62ac2f51ac04e6906d21b16ee62bcc694ade2486

--- common.mk
+++ common.mk
@@ -447,7 +447,6 @@ PRODUCT_COPY_FILES := \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 
