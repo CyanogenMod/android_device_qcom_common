2f043da64a313c9420ab2156a674c7e4d9799fd6
Mingming Yin <mingming@codeaurora.org>
2012-06-26 18:16:25 -0700

    device/qcom/common: Enable audio HAL libraries in the PRODUCT_PACKAGES
    - Audio HAL and PolicyManager libraries are built with optional tag
    - To manadate the compilation of those libraries, they have been
      enabled to the PRODUCT_PACKAGES
    
    Change-Id: I050be4503a19a371fa60d3818e2e252b60188b8a

--- common.mk
+++ common.mk
@@ -37,7 +37,7 @@ ALSA_UCM += snd_soc_msm_Sitar
 ANGLE := libangle
 
 #AUDIO_HARDWARE := audio.primary.mpq8064
-#AUDIO_HARDWARE += audio.primary.msm8960
+AUDIO_HARDWARE += audio.primary.msm8960
 #AUDIO_HARDWARE += audio.primary.msm8660
 #AUDIO_HARDWARE += audio.primary.msm7627_surf
 #AUDIO_HARDWARE += audio.primary.msm7627a
@@ -54,6 +54,7 @@ ANGLE := libangle
 #AUDIO_POLICY += audio_policy.msm7630_surf
 #AUDIO_POLICY += audio_policy.msm7630_fusion
 #AUDIO_POLICY += audio_policy.default
+AUDIO_POLICY += audio_policy.conf
 
 #AMPLOADER
 AMPLOADER := amploader
