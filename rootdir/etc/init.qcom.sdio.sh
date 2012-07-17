8fa48c1c39650a2f7804ac32ce955b5c9b89fd63
Santosh Sajjan <ssajjan@codeaurora.org>
2012-07-18 00:57:38 +0530

    Modified for WiFi Bringup on JB
    Added wifi-sdio-on service ,which will start sdio polling
    Modification of qcom.sdio.sh ,which will always on polling
    
    Change-Id: I1bbd0b056dd571312e8351f0679e3d36fd55f3e4

--- rootdir/etc/init.qcom.sdio.sh
+++ rootdir/etc/init.qcom.sdio.sh
@@ -36,41 +36,41 @@ target=`getprop ro.board.platform`
 
 case "$target" in
     "msm7627_6x")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.1/polling
-        echo "$arg" > /sys/devices/platform/msm_sdcc.2/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.1/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
         ;;
 
     "msm7627_ffa")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.2/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
         ;;
 
     "msm7627_surf")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.1/polling
-        echo "$arg" > /sys/devices/platform/msm_sdcc.2/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.1/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
         ;;
 
     "msm7627a")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.2/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
         ;;
 
     "msm7630_surf")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.3/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.3/polling
         ;;
 
     "msm7630_1x")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.3/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.3/polling
         ;;
 
     "msm7630_fusion")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.3/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.3/polling
         ;;
 
     "msm8660")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.4/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.4/polling
         ;;
 
     "msm8660_csfb")
-        echo "$arg" > /sys/devices/platform/msm_sdcc.4/polling
+        echo 1 > /sys/devices/platform/msm_sdcc.4/polling
         ;;
 esac
 
