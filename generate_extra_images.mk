14ee407de85ca1ab74f34801166d8fbaa9c752b1
Channagoud Kadabi <ckadabi@codeaurora.org>
2012-08-09 22:19:10 +0530

    common: Add support to generate secure boot image
    Change-Id: I04091fe64678c213ef7cdb351cfd053ed12a4747

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -2,6 +2,40 @@
 # persist, device tree & NAND images required for different QCOM targets.
 
 #----------------------------------------------------------------------
+# Generate secure boot image
+#----------------------------------------------------------------------
+ifeq ($(TARGET_BOOTIMG_SIGNED),true)
+INSTALLED_SEC_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img.secure
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
+
+ifneq ($(BUILD_TINY_ANDROID),true)
+intermediates := $(call intermediates-dir-for,PACKAGING,recovery_patch)
+RECOVERY_FROM_BOOT_PATCH := $(intermediates)/recovery_from_boot.p
+endif
+
+ifndef TARGET_SHA_TYPE
+  TARGET_SHA_TYPE := sha256
+endif
+
+define build-boot-image
+	$(hide) mv -f $(1) $(1).nonsecure
+	$(hide) openssl dgst -$(TARGET_SHA_TYPE) -binary $(1).nonsecure > $(1).$(TARGET_SHA_TYPE)
+	$(hide) openssl rsautl -sign -in $(1).$(TARGET_SHA_TYPE) -inkey $(PRODUCT_PRIVATE_KEY) -out $(1).sig
+	$(hide) dd if=/dev/zero of=$(1).sig.padded bs=$(BOARD_KERNEL_PAGESIZE) count=1
+	$(hide) dd if=$(1).sig of=$(1).sig.padded conv=notrunc
+	$(hide) cat $(1).nonsecure $(1).sig.padded > $(1).secure
+	$(hide) rm -rf $(1).$(TARGET_SHA_TYPE) $(1).sig $(1).sig.padded
+	$(hide) mv -f $(1).secure $(1)
+endef
+
+$(INSTALLED_SEC_BOOTIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET) $(RECOVERY_FROM_BOOT_PATCH)
+	$(hide) $(call build-boot-image,$(INSTALLED_BOOTIMAGE_TARGET),$(INTERNAL_BOOTIMAGE_ARGS))
+
+ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_SEC_BOOTIMAGE_TARGET)
+ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_SEC_BOOTIMAGE_TARGET)
+endif
+
+#----------------------------------------------------------------------
 # Generate persist image (persist.img)
 #----------------------------------------------------------------------
 TARGET_OUT_PERSIST := $(PRODUCT_OUT)/persist
