2e7738345d6a56ba75043d98ece037c28afedc00
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2012-09-20 11:47:34 +0530

    common: Use DEVICE_PACKAGE_OVERLAYS
    By using DEVICE_PACKAGE_OVERLAYS, we leave PRODUCT_PACKAGE_OVERLAYS
    for the customers/OEMs to their customization.
    
    CRs-Fixed: 396803
    Change-Id: Ie4d57b9f40002a5c7fc9d0f46a2a68290c282194

--- common.mk
+++ common.mk
@@ -546,7 +546,7 @@ PRODUCT_COPY_FILES += device/qcom/common/media/media_profiles.xml:system/etc/med
 
 # enable overlays to use our version of
 # source/resources etc.
-PRODUCT_PACKAGE_OVERLAYS := device/qcom/common/overlay
+DEVICE_PACKAGE_OVERLAYS := device/qcom/common/overlay
 
 # include additional build utilities
 -include device/qcom/common/utils.mk
