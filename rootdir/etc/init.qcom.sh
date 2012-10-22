0a709fff317ea91c18b12b09274b53c50f0777e0
Sridhar Parasuram <sridhar@codeaurora.org>
2012-10-22 12:23:19 -0700

    Revert "Sensor service: Check for dsps device node to start sensor service"
    This reverts commit 6525f0dc8657562072df8d9a35346cde76e86f67
    
    Change-Id: Ib8900c94c4c0a65877205468af82b5b1392a933b

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -33,22 +33,20 @@ target=`getprop ro.board.platform`
 #
 start_sensors()
 {
-    if [ -c /dev/msm_dsps ]; then
-        mkdir -p /data/system/sensors
-        touch /data/system/sensors/settings
-        chmod 775 /data/system/sensors
-        chmod 664 /data/system/sensors/settings
+    mkdir -p /data/system/sensors
+    touch /data/system/sensors/settings
+    chmod 775 /data/system/sensors
+    chmod 664 /data/system/sensors/settings
 
-        mkdir -p /data/misc/sensors
-        chmod 775 /data/misc/sensors
+    mkdir -p /data/misc/sensors
+    chmod 775 /data/misc/sensors
 
-        if [ ! -s /data/system/sensors/settings ]; then
-            # If the settings file is empty, enable sensors HAL
-            # Otherwise leave the file with it's current contents
-            echo 1 > /data/system/sensors/settings
-        fi
-        start sensors
+    if [ ! -s /data/system/sensors/settings ]; then
+        # If the settings file is empty, enable sensors HAL
+        # Otherwise leave the file with it's current contents
+        echo 1 > /data/system/sensors/settings
     fi
+    start sensors
 }
 
 start_battery_monitor()
