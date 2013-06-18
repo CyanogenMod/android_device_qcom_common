51335dcf12e75bbe3506a24f3c3ddd22d96b1f15
Walter Yang <yandongy@codeaurora.org>
2013-06-18 11:20:45 +0800

    common:add 8610 skuab UCM file
     -add 8610 skuab UCM file
    
    Change-Id: I56e26da576790f77f276a61f6eb8aeb9124553cf

--- common.mk
+++ common.mk
@@ -52,6 +52,7 @@ ALSA_UCM += snd_soc_apq_Taiko_DB
 ALSA_UCM += snd_soc_msm_I2SFusion
 ALSA_UCM += snd_soc_msm_Tapan
 ALSA_UCM += snd_soc_msm_8x10_wcd
+ALSA_UCM += snd_soc_msm_8x10_wcd_skuab
 
 #ANGLE
 ANGLE := libangle
