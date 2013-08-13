edd1402b990c3b62727180f190611c96b5645095
Anji Jonnala <anjir@codeaurora.org>
2013-08-13 22:45:09 +0530

    device: common: Remove thermal symlink script from PRODUCT PACKAGES
    Target detection and thermal config instance which is specific to
    different targets will be added in thermal-engine software as default
    thermal config instances. Remove thermal config symlink script from
    PRODUCT PACKAGES and remove this thermal script execution code from
    init.qcom.syspart_fixup.sh.
    
    Change-Id: Ia823bf68722ede0579f2d5f80d7f915ac8274bba

--- rootdir/etc/init.qcom.syspart_fixup.sh
+++ rootdir/etc/init.qcom.syspart_fixup.sh
@@ -60,11 +60,6 @@ if [ -f /system/etc/init.qcom.mdm_links.sh ]; then
   /system/bin/sh /system/etc/init.qcom.mdm_links.sh
 fi
 
-# Run thermal script
-if [ -f /system/etc/init.qcom.thermal_conf.sh ]; then
-  /system/bin/sh /system/etc/init.qcom.thermal_conf.sh
-fi
-
 # Run wifi script
 if [ -f /system/etc/init.qcom.wifi.sh ]; then
   /system/bin/sh /system/etc/init.qcom.wifi.sh "$target" "$serial"
