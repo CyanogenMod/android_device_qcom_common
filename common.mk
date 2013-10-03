18d4b803b60896275838bc0461e73f820a14cac8
Preeti Ahuja <preetia@codeaurora.org>
2013-10-02 18:20:25 -0700

    common.mk: Do not power down the sim in Airplane Mode.
    When "persist.radio.apm_sim_not_pwdn" property is set, Sim card will
    NOT power down in APM. When this property is not set, which is the current
    default, Sim will be powered down in APM.
    
    Change the default to: Set the property always. So the sim will NOT
    be powered down in APM (Always ON).
    
    Change-Id: Ic26bab3067bf30de92da9ee966b9781494420ed0
    CRs-Fixed: 300064

--- common.mk
+++ common.mk
@@ -692,7 +692,8 @@ PRODUCT_LOCALES := en_US es_US de_DE zh_CN
 PRODUCT_LOCALES += hdpi mdpi
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+    ro.vendor.extension_library=/vendor/lib/libqc-opt.so \
+    persist.radio.apm_sim_not_pwdn=1
 
 PRODUCT_PRIVATE_KEY := device/qcom/common/qcom.key
 
