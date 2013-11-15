bdd42f5bbed91d5d1cd255f1af85ab4eb00f93c0
Muhammed Siju <msiju_qc@codeaurora.org>
2013-11-15 17:29:26 +0530

    Revert "Remove shell scripts for ril init."
    This reverts commit 7395274c37e04257240c6d3e215b6720edc9d190.
    Re introduce ril init shell scripts.This is required since
    "on property" trigger in rc file cannot handle the requirement
    where ril needs to be started based on multisim property
    only once per boot up.As per new requirement, multisim property
    can be changed dynamically between dsds and dsda runtime.
    
    Change multisim property to persist.radio.multisim.config
    
    Change-Id: Ie615baf8211127d6b1eb66d2af3de46eee148588
    CRs-Fixed: 574698

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -114,10 +114,10 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.ril.rc
-LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE       := init.class_main.sh
+LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.ril.rc
+LOCAL_SRC_FILES    := etc/init.class_main.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
