3c5703b59cd3ffac3855fcf24298a80e08aa1725
Dilip Gudlur <dgudlur@codeaurora.org>
2013-11-21 20:18:13 -0800

    init.qcom.post_boot: Performance parameters for 8974 pro
    Enables the following paramters in post boot file for 8974 pro
    1. Synchronize frequencies on thread migration for at least 20ms.
    2. Limit frequency synchronization on thread migration to below 1.7GHz.
    3. Enable SDF for interactive governor for 100 ms.
    
    Change-Id: I6cbde98e16bb00f35a7ee5cc1913968530fa0df9

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -287,6 +287,9 @@ case "$target" in
                 echo 1497600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo "85 1500000:90 1800000:70" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
                 echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+                echo 20 > /sys/module/cpu_boost/parameters/boost_ms
+                echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
+                echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
             ;;
             *)
                 echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
