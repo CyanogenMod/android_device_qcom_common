85eedf8003a02e4eea50b09057e5b582b6eb067d
Dilip Gudlur <dgudlur@codeaurora.org>
2014-01-15 14:24:45 -0800

    power: Add apq8084 specific changes
    Added compilation flag for power-8084.c in
    makefile and detection of apq8084 soc id in
    power.c.
    
    Change-Id: I00e8e18f6de4ee7b38269e4bf565954db46148c5

--- power/Android.mk
+++ power/Android.mk
@@ -36,6 +36,10 @@ ifeq ($(call is-board-platform-in-list, msm8916), true)
 LOCAL_SRC_FILES += power-8916.c
 endif
 
+ifeq ($(call is-board-platform-in-list, apq8084), true)
+LOCAL_SRC_FILES += power-8084.c
+endif
+
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
 LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
 LOCAL_SRC_FILES += ../../../../$(TARGET_POWERHAL_SET_INTERACTIVE_EXT)
