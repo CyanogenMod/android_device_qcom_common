e4f6f5f862900ac5fac0fb9e794afd90ff98b8e8
Amir Vajid <avajid@codeaurora.org>
2014-01-16 13:45:49 -0800

    post_boot: modify hispeed_freq for 8974 pro
    The interactive governor is configured with a hispeed
    frequency of 1.5 GHz. This causes L2 to be bumped up to
    1.5 GHz as well, which requires a turbo CX voltage and
    affects power. To save power when the hispeed frequency
    is triggered, change the hispeed frequency to 1.2 GHz,
    which has the highest L2 frequency that can be supported
    by a nominal CX voltage.
    
    Change-Id: I6742770b79e9d9e929766c1039a73da99b3a89fc

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -290,7 +290,7 @@ case "$target" in
                 echo "interactive" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
                 echo "20000 1400000:40000 1700000:20000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
                 echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-                echo 1497600 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+                echo 1190400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
                 echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
                 echo "85 1500000:90 1800000:70" > /sys/devices/system/cpu/cpufreq/interactive/target_loads
                 echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
