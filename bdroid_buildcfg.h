18f0df1004bc1f4b8fac6706208fb58ef86313ff
Subramanian Srinivasan <subrsrin@codeaurora.org>
2014-02-19 11:59:39 -0800

    Changes Bluedroid configuration for BLE Peripheral mode
    Enables read remote device feature and connection
    update features in bluedroid configuration for
    BLE peripheral mode implementation.
    
    Change-Id: Ifc38df532e6d7b7800097f879d943e950c4f5fd1

--- bdroid_buildcfg.h
+++ bdroid_buildcfg.h
@@ -25,8 +25,8 @@
 // Enables Interleave scan
 #define BTA_HOST_INTERLEAVE_SEARCH  TRUE
 // Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_L2CAP_CHANNELS    14
 // skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
 #endif
