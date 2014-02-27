b734dcb52462c27c9e7788e1537c42a94dd43fbc
Shruthi Krishna <skrish@codeaurora.org>
2014-02-27 14:58:29 -0800

    Removing unsupported targets from lunch combo
    Cleaning up lunch combo by removing unsupported targets
    
    Change-Id: Ib5be7977a8bed551a4e588369ed9e47804a19318

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,11 +27,6 @@
 #  IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-add_lunch_combo msm7627a-userdebug
-add_lunch_combo msm8625-userdebug
-add_lunch_combo msm7630_surf-userdebug
-add_lunch_combo msm8660_surf-userdebug
-add_lunch_combo msm8960-userdebug
 add_lunch_combo msm8974-userdebug
 add_lunch_combo msm8610-userdebug
 add_lunch_combo msm8226-userdebug
