7a6d1a7e85beb034bd8c13059b5c160a5e04fe08
Chiou-Hao Hsu <chsu@codeaurora.org>
2014-03-05 17:06:50 -0800

    common: Add initial support for msm8916_32_k64.
    Add msm8916_32_k64 target in lunch combo.
    
    Change-Id: I5e5a6262c30ca5bd98bbe9dfaf7c95a96f423502

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -35,4 +35,5 @@ add_lunch_combo mpq8092-userdebug
 add_lunch_combo msm_bronze-userdebug
 add_lunch_combo msm8916-userdebug
 add_lunch_combo msm8916_32-userdebug
+add_lunch_combo msm8916_32_k64-userdebug
 add_lunch_combo plutonium-userdebug
