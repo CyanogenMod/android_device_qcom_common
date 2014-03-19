9ba950d28306aa9d1c5ed8f2db7636b6df77fde2
Sravan Kumar Ambapuram <asravan@codeaurora.org>
2014-03-19 10:12:10 +0530

    init.qcom.post_boot: enabling differable timer for ksm
    power savings are observed for less load use cases by
    making ksm timer deferrable.
    
    Change-Id: I734cd89035f9b7ab25ba131c601505910c496d27

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -407,6 +407,7 @@ case "$target" in
         echo 300000 > /sys/devices/system/cpu/cpufreq/ondemand/sync_freq
         echo 80 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold_any_cpu_load
         echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        echo 1 > /sys/kernel/mm/ksm/deferred_timer
         chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         chown -h system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
         chown -h root.system /sys/devices/system/cpu/cpu1/online
