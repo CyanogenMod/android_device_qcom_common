4e89496b595e25cbd61fe7274c27b9c17f02e670
Shashank Mittal <mittals@codeaurora.org>
2014-05-19 18:03:21 -0700

    Power: Fix module path
    Use LOCAL_MODULE_RELATIVE_PATH for 64bit compilation.
    
    Change-Id: I27a437e8e63ffdadb699d5324d223537675e38e3

--- power/Android.mk
+++ power/Android.mk
@@ -6,7 +6,7 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 
