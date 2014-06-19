60ef67e408f9a330a4424f16f3642c7d70d4c6e2
David Ng <dave@codeaurora.org>
2014-06-19 13:20:55 -0700

    build: Fix pick-up of base.mk in common.mk from previous change
    Update common.mk to also pick up base.mk.
    
    Change-Id: I4cb93d1c3d666b042c982b51ecea251cf76568eb

--- common.mk
+++ common.mk
@@ -1,3 +1,5 @@
+$(call inherit-product, device/qcom/common/base.mk)
+
 # For PRODUCT_COPY_FILES, the first instance takes precedence.
 # Since we want use QC specific files, we should inherit
 # device-vendor.mk first to make sure QC specific files gets installed.
