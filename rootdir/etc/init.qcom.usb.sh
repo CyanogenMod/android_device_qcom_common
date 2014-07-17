fab465d8f3cdaac9ec0e0e482a5e5cafae3e4115
Jack Pham <jackp@codeaurora.org>
2014-07-17 14:10:45 -0700

    init.qcom.usb: Disable RNDIS UL aggregation on MSM8994
    MSM8994 IPA hardware does not properly handle aggregated RNDIS
    packets from the host. Set the max_pkt_per_xfer to 1 to disable
    UL aggregation.
    
    Change-Id: Ib36b10a1a6a9920c93bcd93614e3a4dc76caa4b2

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -198,6 +198,8 @@ case "$target" in
     ;;
     "msm8994")
         echo BAM2BAM_IPA > /sys/class/android_usb/android0/f_rndis_qc/rndis_transports
+        echo 1 > /sys/class/android_usb/android0/f_rndis_qc/max_pkt_per_xfer # Disable RNDIS UL aggregation
+    ;;
 esac
 
 #
