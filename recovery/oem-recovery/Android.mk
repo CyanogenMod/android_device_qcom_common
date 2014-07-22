427b76cdf81595f2241c0954a13fc1c508784725
Ameya Thakur <ameyat@codeaurora.org>
2014-07-21 17:32:13 -0700

    recovery: Fix bug where the module would not get compiled in some cases
    We now correctly check the entire TARGET_RECOVERY_UPDATER_LIBS list for
    librecovery_updater_msm.
    
    Change-Id: Ief2bde02b432537ded80ab563e41b82142e15955

--- recovery/oem-recovery/Android.mk
+++ recovery/oem-recovery/Android.mk
@@ -1,7 +1,5 @@
-ifeq ($(TARGET_RECOVERY_UPDATER_LIBS),librecovery_updater_msm)
-
+ifneq ($(filter librecovery_updater_msm,$(TARGET_RECOVERY_UPDATER_LIBS)),)
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := bootable/recovery \
@@ -11,5 +9,4 @@ LOCAL_SRC_FILES := gpt-utils.c dec.c oem-updater.c
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_MODULE := librecovery_updater_msm
 include $(BUILD_STATIC_LIBRARY)
-
 endif
