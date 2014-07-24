21fe90ffd135cc38270a2806b54cd5a200862289
pramod kotreshappa <pramodk@codeaurora.org>
2014-07-23 19:31:10 -0700

    Bluetooth: Push BT permission files to device
    Push Bluetooth and Bluetooth LE permission files for enabling BLE.
    Set MAX_ACL_CONNECTIONS and MAX_L2CAP_CONNECTIONS to 16
    
    Change-Id: I4768a66100c6b131d4421370732c9200fcdedcd8

--- base.mk
+++ base.mk
@@ -696,6 +696,8 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
 
 # Bluetooth configuration files
 #PRODUCT_COPY_FILES += \
