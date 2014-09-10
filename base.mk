65622e3fae001c72a0a68099161a86d75affec2b
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2014-09-10 17:28:16 -0600

    base.mk: Include tcpdump in builds
    Add support for inclusion in user and userdebug variants
    
    CRs-fixed: 665103
    Change-Id: I46171837cde74cd508ffb23a8c8285b99654962e

--- base.mk
+++ base.mk
@@ -700,6 +700,9 @@ PRODUCT_PACKAGES += librecovery_updater_msm
 # vcard jar
 PRODUCT_PACKAGES += vcard
 
+# tcpdump for packet capture
+PRODUCT_PACKAGES += tcpdump
+
 #intialise PRODUCT_PACKAGES_DEBUG list for debug modules
 PRODUCT_PACKAGES_DEBUG :=
 
