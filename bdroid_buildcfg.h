ef33196a9878aae1fe786e3d8dfc1f457e0604fd
Ajay Kumar <ajayku@codeaurora.org>
2014-09-16 12:07:08 +0530

    Bluetooth: GAP: Restricting MAX_ACL_CONNECTIONS to 7
    As per bluetooth specification stack can support max 7 devices
    so restiricting the MAX_ACL_CONNECTIONS that HOST can support to 7
    
    Change-Id: I58abf33083196f2a82a79bf9b152882db2f39ab2
    CRs-Fixed: 724999

--- bdroid_buildcfg.h
+++ bdroid_buildcfg.h
@@ -24,7 +24,7 @@
 #define BTM_DEF_LOCAL_NAME   "QCOM-BTD"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
-#define MAX_ACL_CONNECTIONS    16
+#define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
