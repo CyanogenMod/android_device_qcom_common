d90a5523302dbfcd07204bc5f161fd064315523c
Garik Badalyan <garikb@codeaurora.org>
2014-09-18 12:33:29 -0700

    IMS-VT: Added QTI permissions.
    Added QTI permissions.
    
    Change-Id: I94bca7f2caa124a7ea45ebb2108a102604f86cde

--- base.mk
+++ base.mk
@@ -553,6 +553,9 @@ CHARGER += charger_res_images
 VT_JNI := libvt_jni
 VT_JNI += libimscamera_jni
 
+# VT QTI Permissions
+VT_QTI_PERMISSIONS := qti_permissions.xml
+
 #CRDA
 CRDA := crda
 CRDA += regdbdump
@@ -677,6 +680,7 @@ PRODUCT_PACKAGES += $(UPDATER)
 PRODUCT_PACKAGES += $(WPA)
 PRODUCT_PACKAGES += $(ZLIB)
 PRODUCT_PACKAGES += $(VT_JNI)
+PRODUCT_PACKAGES += $(VT_QTI_PERMISSIONS)
 PRODUCT_PACKAGES += $(CRDA)
 PRODUCT_PACKAGES += $(WLAN)
 
