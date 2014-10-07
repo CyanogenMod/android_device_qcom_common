bc0c87f0dc9133676f6189ad511fc97d4bc6cbdd
Shantanu Jain <shjain@codeaurora.org>
2014-10-07 15:26:26 +0530

    device: common: Add synaptics_dsx.kl to PRODUCT_PACKAGES
    Add key layout file to PRODUCT_PACKAGES so it will be included
    in system image.
    
    Change-Id: I13e78037b61d0384d0fc02f954be3f16edb4dc5b

--- base.mk
+++ base.mk
@@ -265,6 +265,7 @@ KEYPAD += 7x27a_kp.kl
 KEYPAD += 8660_handset.kl
 KEYPAD += atmel_mxt_ts.kl
 KEYPAD += synaptics_rmi4_i2c.kl
+KEYPAD += synaptics_dsx.kl
 KEYPAD += cyttsp-i2c.kl
 KEYPAD += ft5x06_ts.kl
 KEYPAD += ffa-keypad.kl
