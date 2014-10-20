18f7edf5e0bb7b1f05891e3f2fd9db266961d320
guopingy <guopingy@codeaurora.org>
2014-10-20 15:28:00 +0800

    usb uicc: use default persist prefix as sys.*
    use default persist prefix as sys.*
    
    Change-Id: Ie7a66a405a32314407bd2f5b638e8a99c281dd7d

--- rootdir/etc/init.qcom.uicc.sh
+++ rootdir/etc/init.qcom.uicc.sh
@@ -27,7 +27,7 @@
 #
 
 target=`getprop ro.board.platform`
-action=`getprop usb_uicc.enabled`
+action=`getprop sys.usb_uicc.enabled`
 
 if [ -f /sys/devices/soc0/soc_id ]; then
     soc_hwid=`cat /sys/devices/soc0/soc_id`
@@ -102,11 +102,11 @@ uicc_remove()
 case $action in
 "1")
     uicc_insert
-    setprop usb_uicc.loading 1
+    setprop sys.usb_uicc.loading 1
     ;;
 "0")
     uicc_remove
-    setprop usb_uicc.loading 1
+    setprop sys.usb_uicc.loading 1
     ;;
 *)
     echo "USB_UICC invalid action for uicc operation!"
