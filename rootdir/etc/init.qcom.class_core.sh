19bf67a8e7f08ad3a2b515ce3ccf22a54a9196fb
Lalit Kansara <lkansara@codeaurora.org>
2014-11-28 20:01:45 +0530

    Setting fake battery capacity moved from one script to another
    On encrypted device init.qcom.class_core.sh script is unable to write
    fake_batt_capacity to /sys/class/power_supply/battery/capacity file
    due to unknown reason. Moving this logic to init.class_main.sh script
    is working fine
    
    Change-Id: I4c7cc7893314302f4749628d57343be1903e7e1c

--- rootdir/etc/init.qcom.class_core.sh
+++ rootdir/etc/init.qcom.class_core.sh
@@ -197,18 +197,6 @@ case "$target" in
         ;;
 esac
 
-#
-# Allow persistent faking of bms
-# User needs to set fake bms charge in persist.bms.fake_batt_capacity
-#
-fake_batt_capacity=`getprop persist.bms.fake_batt_capacity`
-case "$fake_batt_capacity" in
-    "") ;; #Do nothing here
-    * )
-    echo "$fake_batt_capacity" > /sys/class/power_supply/battery/capacity
-    ;;
-esac
-
 case "$target" in
     "msm7630_surf" | "msm7630_1x" | "msm7630_fusion")
         insmod /system/lib/modules/ss_mfcinit.ko
