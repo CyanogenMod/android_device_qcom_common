55e77aab6a77af3b085c98aab7b5f67758718afc
Nian Sun <nians@codeaurora.org>
2014-12-02 17:03:34 +0800

    Fix compare expression in script.
    Fix compare expression in script. Now all condition
    will pass that makes zram enabled on all platforms.
    
    Change-Id: I7bd449eb43c6e19c19ce9604bcb82a92cf2874b3

--- rootdir/etc/init.qcom.zram.sh
+++ rootdir/etc/init.qcom.zram.sh
@@ -33,6 +33,6 @@ MemTotal=${MemTotalStr:16:8}
 ZRAM_THRESHOLD=1048576
 IsLowMemory=0
 ((IsLowMemory=MemTotal<ZRAM_THRESHOLD?1:0))
-if [ $IsLowMemory ]; then
+if [ $IsLowMemory -eq 1 ]; then
     setprop ro.config.zram true
 fi
