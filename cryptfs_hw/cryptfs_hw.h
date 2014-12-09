84c1af021a8754ce17a09ae3f383e87008c314d6
AnilKumar Chimata <anilc@codeaurora.org>
2014-12-10 01:14:16 +0530

    cryptfs_hw: Add support for wipe_key routine
    Add support for wipe_key routine to clean key.
    
    Change-Id: I9e258e1506d0634c4fc5b5142475005f6eb51c4e

--- cryptfs_hw/cryptfs_hw.h
+++ cryptfs_hw/cryptfs_hw.h
@@ -35,6 +35,7 @@ extern "C" {
 
 unsigned int set_hw_device_encryption_key(const char*, const char*);
 unsigned int update_hw_device_encryption_key(const char*, const char*);
+unsigned int wipe_hw_device_encryption_key(const char*);
 unsigned int is_hw_disk_encryption(const char*);
 
 #ifdef __cplusplus
