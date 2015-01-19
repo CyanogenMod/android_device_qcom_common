f241a4de0186e4640702c599162dc211e8259bb5
Sravan Kumar Ambapuram <asravan@codeaurora.org>
2015-01-19 15:16:53 +0530

    post_boot:msm8916:8939:8909 sched policy to consider avg of windows
    change scheduler policy on L update to consider average of
    last 5 windows instead of max(last window, average of 5 windows).
    It was same on KK (i.e. average of 5 windows) and changed on L
    due to sched code updates. so fixing this on L build as well.
    
    Also,remvoing redundant setting of writing to sched_ravg_hist_size.
    
    Conflicts:
    	rootdir/etc/init.qcom.post_boot.sh
    
    Change-Id: Idc882fc40b9b963fa370c7ce920040f5e7c475c9

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -491,7 +491,7 @@ case "$target" in
         fi
 
         # HMP scheduler settings for 8916, 8936, 8939, 8929
-        echo 2 > /proc/sys/kernel/sched_window_stats_policy
+        echo 3 > /proc/sys/kernel/sched_window_stats_policy
 	echo 3 > /proc/sys/kernel/sched_ravg_hist_size
 
         # Apply governor settings for 8916
@@ -829,7 +829,7 @@ case "$target" in
         fi
 
         # HMP scheduler settings for 8909 similiar to 8916
-        echo 2 > /proc/sys/kernel/sched_window_stats_policy
+        echo 3 > /proc/sys/kernel/sched_window_stats_policy
         echo 3 > /proc/sys/kernel/sched_ravg_hist_size
 
         # HMP Task packing settings for 8909 similiar to 8916
