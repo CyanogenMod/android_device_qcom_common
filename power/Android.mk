b0eb8ec264243d41855923b57ea7916a9a83f90c
Dilip Gudlur <dgudlur@codeaurora.org>
2015-01-28 15:17:45 -0800

    power: introduce 8992-specific handling
    Creates framework for handling special cases like
    display off, video encode differently for 8992.
    
    Change-Id: I195a96f0787c0d2dc2696c75a6f281774e2345cb

--- power/Android.mk
+++ power/Android.mk
@@ -41,6 +41,10 @@ ifeq ($(call is-board-platform-in-list, apq8084), true)
 LOCAL_SRC_FILES += power-8084.c
 endif
 
+ifeq ($(call is-board-platform-in-list, msm8992), true)
+LOCAL_SRC_FILES += power-8992.c
+endif
+
 ifeq ($(call is-board-platform-in-list, msm8994), true)
 LOCAL_SRC_FILES += power-8994.c
 endif
