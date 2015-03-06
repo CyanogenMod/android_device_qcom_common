3a30d6c69ae34fe41f4380818e9c60618cde7c85
Shashank Mittal <mittals@codeaurora.org>
2015-03-05 20:18:01 -0800

    Start qdss agent as a service.
    Start qdss agent as a service and run init.qcom.debug.sh thru debug
    agent script.
    
    Change-Id: I3abde82d0816046e779517bb79b6cbe1ee26cade

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -192,7 +192,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.debug.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := debug
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.debug.sh
 include $(BUILD_PREBUILT)
