94ed698a08bcf78a7692de459c067be540b32603
Steve Kondik <steve@cyngn.com>
2015-03-15 19:54:12 -0500

    power-8226: Remove the HMP boost hint
    Change-Id: I134c2656d806978ef683f70980be27d2bc60eb51

--- power/power-8226.c
+++ power/power-8226.c
@@ -114,7 +114,7 @@ int power_hint_override(__attribute__((unused)) struct power_module *module,
 
     if (hint == POWER_HINT_CPU_BOOST) {
         int duration = (int)data / 1000;
-        int resources[] = { CPUS_ONLINE_MIN_2, SCHED_BOOST_ON, 0x20B, 0x30B, 0x1C00};
+        int resources[] = { CPUS_ONLINE_MIN_2, 0x20B, 0x30B, 0x1C00};
 
         if (duration > 0)
             interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
