663fdb46db7820b8c45b9a92e09b6aedb70b9422
Tanmaya Godbole <tgodbole@codeaurora.org>
2015-03-17 15:35:40 -0700

    Camera: Add permissions for full camera and raw camera
    Fixes the CTS test testCameraFeatures
    
    Change-Id: Idd04087cfc2f927d3ef2eefd9f17ba551f82caaa

--- base.mk
+++ base.mk
@@ -819,6 +819,8 @@ PRODUCT_PACKAGES_DEBUG := init.qcom.testscripts.sh
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml\
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml\
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
