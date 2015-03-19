671192b46d71656eb4ee7d1dc426d517a3f798c7
Girish S Ghongdemath <girishsg@codeaurora.org>
2015-03-19 15:49:29 -0700

    wqmsm8996: Enable Bus DCVS
    Switch devfreq cpu bus dcvs governor to bw_hwmon
    
    Change-Id: I828d2e0982be9c75ba69dbd53d8e28b7ea8f07ee

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1190,5 +1190,10 @@ case "$target" in
         echo 400000 > /proc/sys/kernel/sched_freq_inc_notify
         echo 400000 > /proc/sys/kernel/sched_freq_dec_notify
 
+        # Enable bus-dcvs
+        for devfreq_gov in /sys/class/devfreq/qcom,cpubw*/governor
+        do
+            echo "bw_hwmon" > $devfreq_gov
+        done
     ;;
 esac
