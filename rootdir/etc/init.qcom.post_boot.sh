946704da0826758941aeae8dd9b1b434a20fee6c
Varun Garg <gargv@codeaurora.org>
2015-03-24 15:06:22 +0530

    msm8916: Update packing parameter and their path
    Update new path for mostly_idle_load and mostly_idle_nr_run.
    Also update small_task to 30, mostly_idle_load to 50 and
    mostly_idle_nr_run to 3.
    
    Change-Id: I0283a45b3f515f1f1b084b8772c2bf30c6714d38

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -512,9 +512,15 @@ case "$target" in
                 echo 3 > /proc/sys/kernel/sched_ravg_hist_size
 
                 # HMP Task packing settings for 8916
-                echo 50 > /proc/sys/kernel/sched_small_task
-                echo 30 > /proc/sys/kernel/sched_mostly_idle_load
-                echo 5 > /proc/sys/kernel/sched_mostly_idle_nr_run
+                echo 30 > /proc/sys/kernel/sched_small_task
+                echo 50 > /sys/devices/system/cpu/cpu0/sched_mostly_idle_load
+                echo 50 > /sys/devices/system/cpu/cpu1/sched_mostly_idle_load
+                echo 50 > /sys/devices/system/cpu/cpu2/sched_mostly_idle_load
+                echo 50 > /sys/devices/system/cpu/cpu3/sched_mostly_idle_load
+                echo 3 > /sys/devices/system/cpu/cpu0/sched_mostly_idle_nr_run
+                echo 3 > /sys/devices/system/cpu/cpu1/sched_mostly_idle_nr_run
+                echo 3 > /sys/devices/system/cpu/cpu2/sched_mostly_idle_nr_run
+                echo 3 > /sys/devices/system/cpu/cpu3/sched_mostly_idle_nr_run
 
 		# disable thermal core_control to update scaling_min_freq
                 echo 0 > /sys/module/msm_thermal/core_control/enabled
