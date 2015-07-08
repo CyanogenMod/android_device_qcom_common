ee637c86bac5bbab7d6e25fc9dfef7d6f0a3761c
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2015-07-08 15:32:14 -0700

    common: init: move sensor service to it's own script
    Even though sensor service moved to core class, common script is
    being started as part of late_start services, from which, sensor
    service is started. This is causing issues like, none of the
    sensors are available to clients as System Server also scheduled
    in parallel.
    
    Change-Id: Ie69e43e9845952f06efd9fe52b68b9a02a9522ce

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -210,3 +210,11 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.uicc.sh
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.sensors.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.sensors.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
