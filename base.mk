e1074155a77986f0a0d857ff55a354598f26f2c7
Sai Aitharaju <saia@codeaurora.org>
2015-07-14 20:05:06 +0530

    BTLogKit: Added BTLogKit to Product Packages
      BTLogKit is a new Android Application to set
      the logging trace levels of the Bluetooth
      bluedroid Stack dynamically.
    
      -- This Application is enabled in userdebug
         builds only.
    
    CRs-Fixed: 872836
    Change-Id: Iaf06e622dc372104e9bfc088aad8cb4ae2c901e9

--- base.mk
+++ base.mk
@@ -645,7 +645,8 @@ ifneq ($(TARGET_USES_AOSP),true)
 PRODUCT_PACKAGES += \
        BluetoothExt \
        BTTestApp \
-       HiddTestApp
+       HiddTestApp \
+       BTLogKit
 endif
 
 PRODUCT_PACKAGES += $(ALSA_HARDWARE)
