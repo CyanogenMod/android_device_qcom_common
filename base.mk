4dfeed60ab7f4f0dc4e641fd55ec0f6fa4345189
Sai Aitharaju <saia@codeaurora.org>
2015-08-05 10:15:22 +0530

    BTLogSave: Added BTLogSave to Product Packages
    -- BTLogSave is a new Android Application which
       runs in the background to capture the logcat
       messages. This app is controlled by BTLogKit
       application.
    
    -- This Application is enabled in userdebug builds
       only.
    
    CRs-Fixed: 885022
    Change-Id: Id98cbe772671456002c5d326f6868a5ca48594e6

--- base.mk
+++ base.mk
@@ -646,7 +646,8 @@ PRODUCT_PACKAGES += \
        BluetoothExt \
        BTTestApp \
        HiddTestApp \
-       BTLogKit
+       BTLogKit \
+       BTLogSave
 endif
 
 PRODUCT_PACKAGES += $(ALSA_HARDWARE)
