05f42b1d0245e244c10acd786a7070fd1ca51dac
AUDITYA BHATTARAM <audityab@codeaurora.org>
2015-08-17 15:01:32 +0530

    Disable Jack & Jill compilation for time-being.
    Disable Jack & Jill compilation for time-being.
    
    Change-Id: Ibc3d4731845c932fbdab394f8e385615c2685bc9

--- base.mk
+++ base.mk
@@ -22,6 +22,7 @@ QCOM_BOARD_PLATFORMS += titanium
 QSD8K_BOARD_PLATFORMS := qsd8k
 
 TARGET_USE_VENDOR_CAMERA_EXT := true
+ANDROID_COMPILE_WITH_JACK := false
 
 #List of targets that use video hw
 MSM_VIDC_TARGET_LIST := msm8974 msm8610 msm8226 apq8084 msm8916 msm8994 msm8909 msm8992 msm8996 msm8952 thorium titanium
