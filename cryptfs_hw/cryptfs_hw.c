cf3be1a365c62c6ab8181ddfa7b4881bb7448b54
AnilKumar Chimata <anilc@codeaurora.org>
2015-08-21 00:04:26 +0530

    cryptfs_hw: Update module as per vold project
    Update cryptfs_hw API signature as per the vold project
    request to avoid compilation errors.
    
    Change-Id: I1c2133f3cee395892e7fa160afc6314059ba0bcb

--- cryptfs_hw/cryptfs_hw.c
+++ cryptfs_hw/cryptfs_hw.c
@@ -28,6 +28,7 @@
 
 #include <cryptfs_hw.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/limits.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -203,13 +204,10 @@ unsigned int is_hw_disk_encryption(const char* encryption_mode)
     return ret;
 }
 
-unsigned int wipe_hw_device_encryption_key(const char* enc_mode)
+int clear_hw_device_encryption_key(void)
 {
-    if (!enc_mode)
-        return -1;
-
-    if (is_hw_disk_encryption(enc_mode) && load_qseecom_library())
-        return qseecom_wipe_key(QSEECOM_DISK_ENCRYPTION);
+    if (load_qseecom_library())
+        return qseecom_wipe_key(map_usage(QSEECOM_DISK_ENCRYPTION));
 
     return 0;
 }
