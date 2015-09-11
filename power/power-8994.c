8a0837c48744360b0c5273ad61a64fc74e5fadd4
Steve Kondik <steve@cyngn.com>
2015-09-11 13:33:31 -0700

    power-8994: Adjust options for perf
     * Align with upstream and improve it a little.
    
    Change-Id: I399ece5d220708fc73b0a9e0861b1c4a2a591b7d

--- power/power-8994.c
+++ power/power-8994.c
@@ -80,7 +80,7 @@ static void set_power_profile(int profile) {
             resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
         ALOGD("%s: set powersave", __func__);
     } else if (profile == PROFILE_HIGH_PERFORMANCE) {
-        int resource_values[] = { CPUS_ONLINE_MAX, 0x0901,
+        int resource_values[] = { SCHED_BOOST_ON, CPUS_ONLINE_MAX, 0x0901, 0x101,
             CPU0_MIN_FREQ_TURBO_MAX, CPU1_MIN_FREQ_TURBO_MAX,
             CPU2_MIN_FREQ_TURBO_MAX, CPU3_MIN_FREQ_TURBO_MAX,
             CPU4_MIN_FREQ_TURBO_MAX, CPU5_MIN_FREQ_TURBO_MAX,
@@ -177,7 +177,7 @@ int power_hint_override(__attribute__((unused)) struct power_module *module,
     }
 
     if (hint == POWER_HINT_INTERACTION) {
-        int resources[] = {0x702, 0x20F, 0x30F};
+        int resources[] = { SCHED_BOOST_ON, 0x20D, 0x101, 0x3E01 };
         int duration = 3000;
 
         interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
@@ -186,7 +186,7 @@ int power_hint_override(__attribute__((unused)) struct power_module *module,
 
     if (hint == POWER_HINT_LAUNCH_BOOST) {
         int duration = 2000;
-        int resources[] = { SCHED_BOOST_ON, 0x20D };
+        int resources[] = { SCHED_BOOST_ON, 0x20F, 0x101, 0x3E01 };
 
         interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
 
