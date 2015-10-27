eab3338a0092928c210b08b91bae0475029995db
Achyuth Sai Vadrav <avadra@codeaurora.org>
2015-10-27 13:13:32 +0530

    post_boot: Disable low power modes for msm8952
    Disable low power modes for msm8952.
    
    Change-Id: If975a028d937f19eb5d9dd103e94952533fd24c3

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -336,25 +336,6 @@ case "$target" in
 esac
 
 case "$target" in
-    "msm8952")
-
-	if [ -f /sys/devices/soc0/soc_id ]; then
-		soc_id=`cat /sys/devices/soc0/soc_id`
-	else
-		soc_id=`cat /sys/devices/system/soc/soc0/id`
-	fi
-
-	case "$soc_id" in
-		"264" | "289")
-
-		# Enable low power modes
-		echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
-		;;
-	esac
-	;;
-esac
-
-case "$target" in
     "msm8916")
         if [ -f /sys/devices/soc0/soc_id ]; then
             soc_id=`cat /sys/devices/soc0/soc_id`
@@ -787,8 +768,8 @@ case "$target" in
                 echo 1 > /sys/devices/system/cpu/cpu6/online
                 echo 1 > /sys/devices/system/cpu/cpu7/online
 
-                # Enable Low power modes
-                echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
+                # Keeping Low power modes disabled
+                echo 1 > /sys/module/lpm_levels/parameters/sleep_disabled
 
                 # HMP scheduler (big.Little cluster related) settings
                 echo 93 > /proc/sys/kernel/sched_upmigrate
