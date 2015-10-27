1852c96f21ee70e80ba95b7b875890dabf0fe4ae
Saurabh Shah <saurshah@codeaurora.org>
2015-10-27 11:44:44 -0700

    early_boot: Add permissions for msm_cmd_autorefresh_en
    Add permissions for /sys/class/graphics/fb0/msm_cmd_autorefresh_en
    node. This can be read from / written to by display stack.
    
    Change-Id: Ibe6854d076f176d34986edcde365e3dab91b2660

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -224,6 +224,7 @@ dev_file=/dev/graphics/fb$fb_cnt
         set_perms $file/dyn_pu system.graphics 0664
         set_perms $file/modes system.graphics 0664
         set_perms $file/mode system.graphics 0664
+        set_perms $file/msm_cmd_autorefresh_en system.graphics 0664
     fi
   fi
 done
