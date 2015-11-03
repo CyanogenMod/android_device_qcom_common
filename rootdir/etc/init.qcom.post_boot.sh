9063cfc64821a8315a2f6922da74aa0c334956c4
mohammed thasleem <mthasl@codeaurora.org>
2015-11-03 13:13:05 +0530

    post_boot: msm8909w: Disable core control for 8909w
    Disable core control for 8909w platform since it is based on
    single core. While at it, enable the relevant low memory configurations,
    remove any HMP related settings, and ensure secondary cores are offlined
    only for 8909w.
    
    Change-Id: I325a49e923263c897c679055d38dfb3bfb1ff1a2

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+# Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -1201,37 +1201,53 @@ case "$target" in
 
 	#Set mmcblk0 read_ahead value for 8909_512 target
         ProductName=`getprop ro.product.name`
-        if [ "$ProductName" == "msm8909_512" ]; then
-            echo 128 > /sys/block/mmcblk0/queue/read_ahead_kb
+	if [ "$ProductName" == "msm8909_512" ]; then
+		echo 128 > /sys/block/mmcblk0/queue/read_ahead_kb
 	fi
 
         #Enable adaptive LMK and set vmpressure_file_min
         ProductName=`getprop ro.product.name`
-        if [ "$ProductName" == "msm8909" ] || [ "$ProductName" == "msm8909_LMT" ]; then
+	if [ "$ProductName" == "msm8909" ] || [ "$ProductName" == "msm8909_LMT" ]; then
 		echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-	        echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-        elif [ "$ProductName" == "msm8909_512" ]; then
+		echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+	elif [ "$ProductName" == "msm8909_512" ] || [ "$ProductName" == "msm8909w" ]; then
 		echo "8192,11264,14336,17408,20480,26624" > /sys/module/lowmemorykiller/parameters/minfree
 		echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 		echo 32768 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
-        fi
+	fi
 
-        # HMP scheduler settings for 8909 similiar to 8916
-        echo 3 > /proc/sys/kernel/sched_window_stats_policy
-        echo 3 > /proc/sys/kernel/sched_ravg_hist_size
+	if [ "$ProductName" != "msm8909w" ]; then
+		# HMP scheduler settings for 8909 similiar to 8916
+		echo 3 > /proc/sys/kernel/sched_window_stats_policy
+		echo 3 > /proc/sys/kernel/sched_ravg_hist_size
 
-        # HMP Task packing settings for 8909 similiar to 8916
-        echo 20 > /proc/sys/kernel/sched_small_task
-        echo 30 > /proc/sys/kernel/sched_mostly_idle_load
-        echo 3 > /proc/sys/kernel/sched_mostly_idle_nr_run
+		# HMP Task packing settings for 8909 similiar to 8916
+		echo 20 > /proc/sys/kernel/sched_small_task
+		echo 30 > /proc/sys/kernel/sched_mostly_idle_load
+		echo 3 > /proc/sys/kernel/sched_mostly_idle_nr_run
+	fi
 
         # disable thermal core_control to update scaling_min_freq
         echo 0 > /sys/module/msm_thermal/core_control/enabled
         echo 1 > /sys/devices/system/cpu/cpu0/online
-        echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+
+	if [ "$ProductName" == "msm8909w" ]; then
+		echo "performance" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+		echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+		echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+		#Below entries are to set the GPU frequency and DCVS governor
+		echo 200000000 > /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
+		echo 200000000 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+		echo performance > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+	else
+		echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+		echo 800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	fi
+
         # enable thermal core_control now
-        echo 1 > /sys/module/msm_thermal/core_control/enabled
+	if [ "$ProductName" != "msm8909w" ]; then
+		echo 1 > /sys/module/msm_thermal/core_control/enabled
+	fi
 
         echo "30000 1094400:50000" > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
         echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
@@ -1242,22 +1258,31 @@ case "$target" in
         echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
         echo 50000 > /sys/devices/system/cpu/cpufreq/interactive/sampling_down_factor
 
-        # Bring up all cores online
-        echo 1 > /sys/devices/system/cpu/cpu1/online
-	echo 1 > /sys/devices/system/cpu/cpu2/online
-	echo 1 > /sys/devices/system/cpu/cpu3/online
+	if [ "$ProductName" == "msm8909w" ]; then
+		# Post boot, have only cpu0 online. Make all other cores go offline
+		echo 0 > /sys/devices/system/cpu/cpu1/online
+		echo 0 > /sys/devices/system/cpu/cpu2/online
+		echo 0 > /sys/devices/system/cpu/cpu3/online
+	else
+		# Bring up all cores online
+		echo 1 > /sys/devices/system/cpu/cpu1/online
+		echo 1 > /sys/devices/system/cpu/cpu2/online
+		echo 1 > /sys/devices/system/cpu/cpu3/online
+	fi
+
 	echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 
 	# Enable core control
-	insmod /system/lib/modules/core_ctl.ko
-	echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
-        max_freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq`
-        min_freq=800000
-        echo $((min_freq*100 / max_freq)) $((min_freq*100 / max_freq)) $((66*1000000 / max_freq)) \
-	$((55*1000000 / max_freq)) > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
-        echo $((33*1000000 / max_freq)) > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-	echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
-
+	if [ "$ProductName" != "msm8909w" ]; then
+		insmod /system/lib/modules/core_ctl.ko
+		echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
+		max_freq=`cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq`
+		min_freq=800000
+		echo $((min_freq*100 / max_freq)) $((min_freq*100 / max_freq)) $((66*1000000 / max_freq)) \
+		$((55*1000000 / max_freq)) > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
+		echo $((33*1000000 / max_freq)) > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
+		echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
+	fi
 
         # Apply governor settings for 8909
 	for devfreq_gov in /sys/class/devfreq/qcom,cpubw*/governor
