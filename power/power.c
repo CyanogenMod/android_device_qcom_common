389272cb28f7d4c7676bf0659f1a3a780cbe0f74
Keith Mok <kmok@cyngn.com>
2015-11-13 09:46:14 -0800

    Refactor TARGET_TAP_TO_WAKE_NODE
    Add TARGET_POWER_SET_FEATURE_LIB, since other devices
    are not simply writing a 0/1 to a device node.
    TARGET_TAP_TO_WAKE_NODE still valid and both
    TARGET_TAP_TO_WAKE_NODE and TARGET_POWER_SET_FEATURE_LIB are
    allowed to be coexist. (Currently set_feature only got double tap wake,
    but may be extended in future)
    
    Change-Id: I5c6204fd38a6fbe48e8aac49843bfa047c4b6436

--- power/power.c
+++ power/power.c
@@ -49,6 +49,7 @@
 #include "hint-data.h"
 #include "performance.h"
 #include "power-common.h"
+#include "power-feature.h"
 
 static int saved_dcvs_cpu0_slack_max = -1;
 static int saved_dcvs_cpu0_slack_min = -1;
@@ -464,15 +465,17 @@ out:
     pthread_mutex_unlock(&hint_mutex);
 }
 
-void set_feature(struct power_module *module __unused, feature_t feature, int state)
+void set_feature(struct power_module *module, feature_t feature, int state)
 {
-    char tmp_str[NODE_MAX];
 #ifdef TAP_TO_WAKE_NODE
+    char tmp_str[NODE_MAX];
     if (feature == POWER_FEATURE_DOUBLE_TAP_TO_WAKE) {
         snprintf(tmp_str, NODE_MAX, "%d", state);
         sysfs_write(TAP_TO_WAKE_NODE, tmp_str);
+        return;
     }
 #endif
+    set_device_specific_feature(module, feature, state);
 }
 
 int get_feature(struct power_module *module __unused, feature_t feature)
