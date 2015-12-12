2ffe59b6e38131212c683183af88a542ddd606fd
Tapas Kumar Kundu <tkundu@codeaurora.org>
2015-12-11 20:04:46 -0800

    init.qcom.post_boot: Enable cpufreq governor settings for mincpubw
    Enable cpufreq governor setting for mincpubw on msm8996 since memory
    latency governor along with bw_hwmon governor is not sufficient for
    voting for higher DDR frequencies for most memory bound workloads.
    
    Change-Id: I4da268b268c64b43d92f40b872c7ba61e4f3d465

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -1608,6 +1608,7 @@ case "$target" in
             echo "mem_latency" > $memlat/governor
             echo 10 > $memlat/polling_interval
         done
+        echo "cpufreq" > /sys/class/devfreq/soc:qcom,mincpubw/governor
 
 	soc_revision=`cat /sys/devices/soc0/revision`
 	if [ "$soc_revision" == "1.0" ] || [ "$soc_revision" == "2.0" ]; then
