e346713fa103776bee1b724da68cc048591b5362
Mohammed Thasleem <mthasl@codeaurora.org>
2016-02-03 19:18:59 +0530

    msm8909w: Remove USB_HOST feature
    Wearables should not support this feature.
    
    Change-Id: I1ba3def9126120cc914b57e0ae074c2821f658d7

--- base.mk
+++ base.mk
@@ -785,9 +785,13 @@ PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+
+ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+endif
 
 # Bluetooth configuration files
 #PRODUCT_COPY_FILES += \
