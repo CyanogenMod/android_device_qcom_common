f39b76e42377c78eedd41856970d919e0686a228
Sathish Ambley <sathishambley@codeaurora.org>
2016-03-24 15:47:09 -0700

    early_boot: Add permissions for lineptr_value node
    Add permissions for /sys/class/graphics/fb0/lineptr_value node.
    This node will be written by the VR service.
    
    Change-Id: I4cbef63ccb91b0a26d98a383e350779ea3227ea2

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -244,6 +244,9 @@ dev_file=/dev/graphics/fb$fb_cnt
         set_perms $file/modes system.graphics 0664
         set_perms $file/mode system.graphics 0664
         set_perms $file/msm_cmd_autorefresh_en system.graphics 0664
+        if [ -f $file/lineptr_value ]; then
+            set_perms $file/lineptr_value system.graphics 0664
+        fi
     fi
   fi
 done
