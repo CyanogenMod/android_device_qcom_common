3d8867f214f63ad28a66109947432f83c1a0500b
Scott Mertz <scott@cyngn.com>
2016-04-19 09:11:09 -0700

    common: disable verity
    We don't support it yet, so disable it temporarily
    
    Change-Id: I18e404e02bf8e7c75c8c304382b4dd35f3fd9868

--- base.mk
+++ base.mk
@@ -877,10 +877,6 @@ PRODUCT_PACKAGE_OVERLAYS += device/qcom/common/product/overlay
 #Enabling video for live effects
 -include frameworks/base/data/videos/VideoPackage1.mk
 
-# dm-verity definitions
-PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
-
 #skip boot jars check
 SKIP_BOOT_JARS_CHECK := true
 
