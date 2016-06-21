4e28efc2d103918f241e4988fcffe159cca6ecea
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-06-21 10:44:49 +0800

    power: msm8996: Support boost and perf profile hints
    Change-Id: I010d4d1d3fb495e523b065c11ca95c4305530f9f

--- power/power-common.h
+++ power/power-common.h
@@ -42,6 +42,9 @@
 #define HINT_HANDLED (0)
 #define HINT_NONE (-1)
 
+#define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
+#define UNUSED          __attribute__((unused))
+
 enum CPU_GOV_CHECK {
     CPU0 = 0,
     CPU1 = 1,
