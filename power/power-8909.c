25bc8764c184cf02b0076ecd4d0c933293bee3b9
Pat Erley <perley@cyngn.com>
2016-06-22 14:05:00 -0700

    msm8909: Add basic power hal
    This is an initial power hal for msm8909 socs.  It will allow
    users to have better control over battery life vs performance.
    
    The default behavior on the device it was tested on matches the
    values used in the Performance Biased mode.
    
    Change-Id: I2be9f7684e65db3dc3decb55ff34c19d5acce6ef

--- power/power-8909.c
+++ power/power-8909.c
@@ -47,6 +47,54 @@
 #include "performance.h"
 #include "power-common.h"
 
+static int display_hint_sent;
+static int display_hint2_sent;
+static int first_display_off_hint;
+extern int display_boost;
+
+static int current_power_profile = PROFILE_BALANCED;
+
+int get_number_of_profiles() {
+    return 5;
+}
+
+
+static void set_power_profile(int profile) {
+
+    if (profile == current_power_profile)
+        return;
+
+    ALOGV("%s: profile=%d", __func__, profile);
+
+    if (current_power_profile != PROFILE_BALANCED) {
+        undo_hint_action(DEFAULT_PROFILE_HINT_ID);
+        ALOGV("%s: hint undone", __func__);
+    }
+
+    if (profile == PROFILE_HIGH_PERFORMANCE) {
+        int resource_values[] = { CPUS_ONLINE_MIN_4, 0x20E, 0x150E };
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID,
+            resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+        ALOGD("%s: set performance mode", __func__);
+    } else if (profile == PROFILE_BIAS_PERFORMANCE) {
+        int resource_values[] = { 0x207, 0x150E };
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID,
+            resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+        ALOGD("%s: set bias perf mode", __func__);
+    } else if (profile == PROFILE_BIAS_POWER) {
+        int resource_values[] = { 0x201, 0x1509 };
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID,
+            resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+        ALOGD("%s: set bias power mode", __func__);
+    } else if (profile == PROFILE_POWER_SAVE) {
+        int resource_values[] = { CPUS_ONLINE_MAX_LIMIT_2, 0x201, 0x1509 };
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID,
+            resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+        ALOGD("%s: set powersave", __func__);
+    }
+
+    current_power_profile = profile;
+}
 
 static void process_video_encode_hint(void *metadata)
 {
@@ -90,18 +138,144 @@ static void process_video_encode_hint(void *metadata)
     }
 }
 
-int power_hint_override(struct power_module *module, power_hint_t hint, void *data)
+extern void interaction(int duration, int num_args, int opt_list[]);
+
+int power_hint_override(__unused struct power_module *module, power_hint_t hint, void *data)
 {
+    if (hint == POWER_HINT_SET_PROFILE) {
+        set_power_profile(*(int32_t *)data);
+        return HINT_HANDLED;
+    }
+
+    // Skip other hints in high/low power modes
+    if (current_power_profile == PROFILE_POWER_SAVE ||
+        current_power_profile == PROFILE_HIGH_PERFORMANCE) {
+        return HINT_HANDLED;
+    }
+
     switch(hint) {
+        case POWER_HINT_LAUNCH_BOOST:
+        {
+            int duration = 2000;
+            int resources[] = { CPUS_ONLINE_MIN_3, 0x20E, 0x150E };
+
+            interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
+
+            return HINT_HANDLED;
+        }
+        case POWER_HINT_CPU_BOOST:
+        {
+            int duration = *(int32_t *)data / 1000;
+            int resources[] = { CPUS_ONLINE_MIN_2, 0x20E, 0x150C };
+
+            if (duration)
+                interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
+
+            return HINT_HANDLED;
+        }
+        case POWER_HINT_INTERACTION:
+        {
+            int duration = 500, duration_hint = 0;
+            static unsigned long long previous_boost_time = 0;
+
+            if (data) {
+                duration_hint = *((int *)data);
+            }
+
+            duration = duration_hint > 0 ? duration_hint : 500;
+
+            struct timeval cur_boost_timeval = {0, 0};
+            gettimeofday(&cur_boost_timeval, NULL);
+            unsigned long long cur_boost_time = cur_boost_timeval.tv_sec * 1000000
+                                                + cur_boost_timeval.tv_usec;
+            unsigned long long elapsed_time = (double)(cur_boost_time - previous_boost_time);
+            if (elapsed_time > 750000)
+                elapsed_time = 750000;
+            // don't hint if it's been less than 250ms since last boost
+            // also detect if we're doing anything resembling a fling
+            // support additional boosting in case of flings
+            else if (elapsed_time < 250000 && duration <= 750)
+                return HINT_HANDLED;
+
+            previous_boost_time = cur_boost_time;
+
+            int resources[] = { (duration >= 2000 ? CPUS_ONLINE_MIN_3 : CPUS_ONLINE_MIN_2),
+                                0x20C, 0x150E };
+
+            if (duration)
+                interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
+
+            return HINT_HANDLED;
+        }
         case POWER_HINT_VIDEO_ENCODE:
         {
-          process_video_encode_hint(data);
-          return HINT_HANDLED;
+            process_video_encode_hint(data);
+            return HINT_HANDLED;
         }
         default:
-        {
             break;
+    }
+    return HINT_NONE;
+}
+
+int set_interactive_override(struct power_module *module __unused, int on)
+{
+    char governor[80];
+
+    if (get_scaling_governor(governor, sizeof(governor)) == -1) {
+        ALOGE("Can't obtain scaling governor.");
+
+        return HINT_NONE;
+    }
+
+    if (!on) {
+        /* Display off. */
+        /*
+         * We need to be able to identify the first display off hint
+         * and release the current lock holder
+         */
+        if (display_boost) {
+            if (!first_display_off_hint) {
+                undo_initial_hint_action();
+                first_display_off_hint = 1;
+            }
+            /* used for all subsequent toggles to the display */
+            if (!display_hint2_sent) {
+                undo_hint_action(DISPLAY_STATE_HINT_ID_2);
+                display_hint2_sent = 1;
+            }
+        }
+
+        if ((strncmp(governor, ONDEMAND_GOVERNOR, strlen(ONDEMAND_GOVERNOR)) == 0) &&
+                (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
+            int resource_values[] = { MS_500, SYNC_FREQ_600, OPTIMAL_FREQ_600,
+                                      THREAD_MIGRATION_SYNC_OFF };
+
+            if (!display_hint_sent) {
+                perform_hint_action(DISPLAY_STATE_HINT_ID,
+                        resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+                display_hint_sent = 1;
+            }
+
+            return HINT_HANDLED;
+        }
+    } else {
+        /* Display on */
+        if (display_boost && display_hint2_sent) {
+            int resource_values2[] = {CPUS_ONLINE_MIN_2};
+            perform_hint_action(DISPLAY_STATE_HINT_ID_2,
+                    resource_values2, sizeof(resource_values2)/sizeof(resource_values2[0]));
+            display_hint2_sent = 0;
+        }
+
+        if ((strncmp(governor, ONDEMAND_GOVERNOR, strlen(ONDEMAND_GOVERNOR)) == 0) &&
+                (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
+            undo_hint_action(DISPLAY_STATE_HINT_ID);
+            display_hint_sent = 0;
+
+            return HINT_HANDLED;
         }
     }
+
     return HINT_NONE;
 }
