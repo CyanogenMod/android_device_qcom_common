878c7e98b6cf89d7cfd09df55a3c80b2576095c9
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-06-29 23:00:14 +0800

    power: Always allow powersave mode
    Even if the PowerHAL doesn't properly handle the powersave profile,
    users can still enable the battery-saving tweaks in the framework.
    
    Change-Id: I054f3ef05d67684a75e693957b3ec47b8ae486ca

--- power/power.c
+++ power/power.c
@@ -246,7 +246,7 @@ int __attribute__ ((weak)) set_interactive_override(
 
 int __attribute__ ((weak)) get_number_of_profiles()
 {
-    return 0;
+    return 1;
 }
 
 #ifdef SET_INTERACTIVE_EXT
@@ -399,11 +399,11 @@ void set_interactive(struct power_module *module, int on)
                 (strlen(governor) == strlen(ONDEMAND_GOVERNOR))) {
             undo_hint_action(DISPLAY_STATE_HINT_ID);
             display_hint_sent = 0;
-        } else if ((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) &&
+        } else if ((strncmp(governor, INTERACTIVE_GOVERNOR, strlen(INTERACTIVE_GOVERNOR)) == 0) && 
                 (strlen(governor) == strlen(INTERACTIVE_GOVERNOR))) {
             undo_hint_action(DISPLAY_STATE_HINT_ID);
             display_hint_sent = 0;
-        } else if ((strncmp(governor, MSMDCVS_GOVERNOR, strlen(MSMDCVS_GOVERNOR)) == 0) && 
+        } else if ((strncmp(governor, MSMDCVS_GOVERNOR, strlen(MSMDCVS_GOVERNOR)) == 0) &&
                 (strlen(governor) == strlen(MSMDCVS_GOVERNOR))) {
             if (saved_interactive_mode == -1 || saved_interactive_mode == 0) {
                 /* Display turned on. Restore if possible. */
