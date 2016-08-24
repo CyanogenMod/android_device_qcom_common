d9d70f984f2b64bb8e1ef40deed240961e271059
paranoiagu <gu.yanfeng@gmail.com>
2016-08-24 12:50:49 +0800

    power: msm8916: Fix issue with CPU0 going offline
    After commit ad90ded254d9273ca545753415deb9be66e847f4,
    CPU0 goes offline after some device uptime.
    
    When the device is just booted up, CPU0 works fine. After a while
    however, CPU0 just goes offline and never comes back online again.
    
    It seems like the order of the ops in the array matters.
    
    Change-Id: Ie568ecd7ae65033d8670c1a4ec5758b485e65c8c

--- power/power-8916.c
+++ power/power-8916.c
@@ -384,17 +384,17 @@ int power_hint_override(struct power_module *module __unused, power_hint_t hint,
 
         if (duration >= 1500) {
             int resources[] = {
-                ALL_CPUS_PWR_CLPS_DIS,
                 SCHED_BOOST_ON,
+                0x20D,
+                ALL_CPUS_PWR_CLPS_DIS,
                 SCHED_PREFER_IDLE_DIS,
-                0x20D
             };
             interaction(duration, ARRAY_SIZE(resources), resources);
         } else {
             int resources[] = {
+                0x20D,
                 ALL_CPUS_PWR_CLPS_DIS,
                 SCHED_PREFER_IDLE_DIS,
-                0x20D
             };
             interaction(duration, ARRAY_SIZE(resources), resources);
         }
@@ -404,11 +404,11 @@ int power_hint_override(struct power_module *module __unused, power_hint_t hint,
     if (hint == POWER_HINT_LAUNCH_BOOST) {
         int duration = 2000;
         int resources[] = {
-            ALL_CPUS_PWR_CLPS_DIS,
             SCHED_BOOST_ON,
-            SCHED_PREFER_IDLE_DIS,
             0x20F,
+            ALL_CPUS_PWR_CLPS_DIS,
             0x1C00,
+            SCHED_PREFER_IDLE_DIS,
             0x4001,
             0x4101,
             0x4201
@@ -422,10 +422,10 @@ int power_hint_override(struct power_module *module __unused, power_hint_t hint,
     if (hint == POWER_HINT_CPU_BOOST) {
         int duration = *(int32_t *)data / 1000;
         int resources[] = {
-            ALL_CPUS_PWR_CLPS_DIS,
             SCHED_BOOST_ON,
+            0x20D,
             SCHED_PREFER_IDLE_DIS,
-            0x20D
+            ALL_CPUS_PWR_CLPS_DIS,
         };
 
         if (duration > 0)
