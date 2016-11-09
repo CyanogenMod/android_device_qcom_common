3d9d25c227a9ada3d9d6201c58bfce83eedcf24f
Steve Kondik <steve@cyngn.com>
2016-11-08 21:49:27 -0800

    qc: Add guard makefile for binaries repo
    Change-Id: Ia5c178ed357b43120838d03b2ef4c24b3d242a36

--- extractors/setup-makefiles.sh
+++ extractors/setup-makefiles.sh
@@ -176,3 +176,14 @@ echo "endif" >> "$PRODUCTMK"
 
 # We are done!
 write_footers
+
+# Add a guard on the top level
+cat << EOF > "$CM_ROOT/vendor/$VENDOR/$DEVICE/Android.mk"
+LOCAL_PATH := \$(call my-dir)
+
+include \$(CLEAR_VARS)
+
+ifeq (\$(BOARD_USES_QCOM_HARDWARE),true)
+include \$(call all-makefiles-under,\$(LOCAL_PATH))
+endif
+EOF
