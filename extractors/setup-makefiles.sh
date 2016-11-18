4b8412d13a3f0500dd9a737178793ad186930b30
Steve Kondik <steve@cyngn.com>
2016-11-18 13:39:33 -0800

    qc: Fix makefiles
    Change-Id: Ice1905857a1af24933fefe36090c91bfd66e02f9

--- extractors/setup-makefiles.sh
+++ extractors/setup-makefiles.sh
@@ -184,6 +184,6 @@ LOCAL_PATH := \$(call my-dir)
 include \$(CLEAR_VARS)
 
 ifeq (\$(BOARD_USES_QCOM_HARDWARE),true)
-include \$(call all-makefiles-under,\$(LOCAL_PATH))
+include \$(call all-makefiles-under,\$(LOCAL_PATH)/\$(TARGET_BOARD_PLATFORM))
 endif
 EOF
